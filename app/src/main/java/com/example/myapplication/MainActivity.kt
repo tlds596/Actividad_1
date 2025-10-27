4adefd60fbc298399fa8a0468164ba669c45601a
Jhon Harvey <tlds596@gmail.com>
2025-10-27 10:47:39 -0500

    Actividad 1 27/10

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.myapplication
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun Home () {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){ Column(
+        modifier = Modifier.fillMaxSize().border(1.dp, Color.Black),
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().height(20.dp).border(1.dp, Color.Black),
+        ){}
+        Row(
+            modifier = Modifier.fillMaxWidth().height(60.dp).border(1.dp, Color.Black),
+
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ){
+            Column(){
+                Text(text = "1")
+                Text(text = "2")
+            }
+            Column{
+                Text(text = "3")
+                Text(text = "4")
+            }
+            Column{
+                Text(text = "5")
+                Text(text = "6")
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth().height(20.dp).border(1.dp, Color.Black),
+        ){}
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable()
+fun preview (){
+    Home()
+}
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyApplicationTheme {
+//        Greeting("Android")
+//    }
+//}
+
